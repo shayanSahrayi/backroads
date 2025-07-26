5f7f045186de81d2546f3ee5fc8c4130d9e3237b
shayan sahraei <shayan.sh1996@gmial.com>
2025-07-26 15:11:36 +0330

    fixed aboute

--- src/components/About.js
+++ src/components/About.js
@@ -23,7 +23,7 @@ const About=()=>{
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#" className="btn">read more</a>
+          <a href="/ss" className="btn">read more</a>
         </article>
       </div>
     </section>
